6f8d318ebb157af909b3f9f13a71a290201a28b0
HADHRI <hadhri.anas@gmail.com>
2019-12-13 12:23:59 +0100

    Requesting Category Spring Back end api from Category component

---
+++ app/src/Category.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+
+
+class  Category extends Component {
+    //Internal storage of a component 
+    //Equivalent to private fields in java 
+    state = { 
+        isLoading : true,
+        Categories :[]
+     }
+     //Async function 
+     //when the component is mounted , make this async calll the spring boot
+     //React app is on 3030 port or spring boot app turns on 8080 port 
+     //so in order to resolve this problem , we use a Proxy 
+     async componentDidMount(){
+        const response = await fetch('/api/categories');
+        const body=await response.json();
+        this.setState({Categories:body , isLoading:false})
+     } 
+
+    render() { 
+        const {Categories,isLoading} = this.state;
+        if(isLoading){
+            return (<div>Loading ...</div>)
+        }
+        return ( 
+            <div>
+                <h2>Categories</h2>
+                {
+                    Categories.map(Category =>
+                        <div id={Category.id}>
+                            {Category.name}
+                        </div>
+  )
+                }
+            </div>
+
+         );
+    }
+}
+ 
+export default Category;
